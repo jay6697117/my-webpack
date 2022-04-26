10d21e4624abaa1773e9558909f6cad9ceca4a2f
steve zhang <871506263@qq.com>
2022-04-26 14:16:36 +0800

    提交代码于: 2022-04-26 14:16:36

---
+++ webpack.prod.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  // mode: 'production',
+  mode: 'development',
+  //默认 false，也就是不开启
+  watch: true,
+  //只有开启监听模式时，watchOptions才有意义
+  watchOptions: {
+    //默认为空，不监听的文件或者文件夹，支持正则匹配
+    ignored: /node_modules/,
+    //监听到变化发生后会等300ms再去执行，默认300ms
+    aggregateTimeout: 300,
+    //判断文件是否发生变化是通过不停询问系统指定文件有没有变化实现的，默认每秒问1000次
+    poll: 1000
+  },
+  //入口
+  entry: { app: './src/index.js', search: './src/search.js' },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.(le|c)ss$/,
+        use: ['style-loader', 'css-loader', 'less-loader']
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10240 // 10240 byte === 10k byte
+            }
+          }
+        ]
+      },
+      {
+        test: /.(woff|woff2|eot|ttf|otf)$/,
+        use: 'file-loader'
+      }
+    ]
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  devServer: {
+    port: 8080,
+    contentBase: './dist',
+    hot: true
+  }
+};
